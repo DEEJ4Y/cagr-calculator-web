f254ffbd11923bbaeb533fbbac1ab4cf20c34d88
David Joseph <70807315+DEEJ4Y@users.noreply.github.com>
2024-04-09 21:50:52 +0530

    Stylize delete trades button

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import useCalculatedXirr from '@/hooks/useCalculatedXirr';
-import { Button, Stack, Title, Text } from '@mantine/core';
+import { Button, Stack, Title, Text, Group } from '@mantine/core';
 import Link from 'next/link';
 
 export default function Home() {
@@ -9,16 +9,18 @@ export default function Home() {
     <Stack p="md">
       <Title>XIRR Calculator</Title>
       <Text>View XIRR for realized trades from Zerodha or Groww.</Text>
-      <Link href="/upload">
-        <Button variant="filled">Upload</Button>
-      </Link>
-      <Button
-        variant="outline"
-        color="red"
-        onClick={() => localStorage.setItem('allTrades', '[]')}
-      >
-        Reset Saved Trades
-      </Button>
+      <Group>
+        <Link href="/upload">
+          <Button variant="filled">Upload</Button>
+        </Link>
+        <Button
+          variant="outline"
+          color="red"
+          onClick={() => localStorage.setItem('allTrades', '[]')}
+        >
+          Reset Saved Trades
+        </Button>
+      </Group>
       Your XIRR is: {(100 * xirr).toFixed(2)}%
     </Stack>
   );
